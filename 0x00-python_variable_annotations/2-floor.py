53ee42f5b656db0fae14c775176cdcc34cd1f38f
Diego Linares <diegojeanluck@hotmail.com>
2023-01-23 15:03:35 -0500

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function floor which
+takes a float n as argument and
+returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Round to bottom a float
+
+    Args:
+        n (float): Float number to be rounded
+
+    Returns:
+        int: The number rounded.
+    """
+    return (int(n))
