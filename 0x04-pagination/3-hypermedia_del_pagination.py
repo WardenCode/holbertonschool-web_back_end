cdcfc91d73e0b1e706a5eddc602153c0f2b1376b
Diego Linares <diegojeanluck@hotmail.com>
2023-01-31 16:10:37 -0500

    Try something

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -5,25 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import Dict, List, Tuple, Union
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Get the start and end index of the solicited page
-
-    Args:
-        page (int): Actual page
-        page_size (int): Size of each page
-
-    Returns:
-        Tuple[int, int]: Tuple that contains the
-        start and end index of the solicited page
-    """
-    first_page_element_idx: int = (page - 1) * page_size
-    last_page_element_idx: int = page * page_size
-
-    return (first_page_element_idx, last_page_element_idx)
+from typing import Dict, List
 
 
 class Server:
@@ -57,80 +39,28 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        Get a list of rows, using pagination from the DATA_FILE
-
-        Args:
-            page (int, optional): Page to select. Defaults to 1.
-            page_size (int, optional): Size of the page. Defaults to 10.
-
-        Returns:
-            List[List]: List of rows of DATA_FILE
+        get_hyper_index method
         """
-
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page > 0
-
-        init, end = index_range(page, page_size)
-
-        return (self.dataset()[init: end])
-
-    def get_hyper(self, page: int = 1,
-                  page_size: int = 10) -> Dict[str, Union[int, List, None]]:
-        """
-        Get a Hyper media pagination response
-
-        Args:
-            page (int, optional): Page requested. Defaults to 1.
-            page_size (int, optional): Size of each page. Defaults to 10.
-
-        Returns:
-            Dict[str, Union[int, List, None]]: Hyper media pagination
-            with data
-        """
-
-        data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
-
-        return ({
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if (page + 1) <= total_pages else None,
-            "prev_page": page - 1 if (page - 1) > 0 else None,
-            "total_pages": total_pages,
-        })
-
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
-        """
-        Get page size elements from index, regardless if any were deleted
-
-        Args:
-            index (int, optional): Index to get. Defaults to None.
-            page_size (int, optional): Size of Page. Defaults to 10.
-
-        Returns:
-            Dict: Data from index to index + page_size
-        """
-        all_data = self.indexed_dataset()
-        total_len = len(all_data)
-        assert (index <= total_len)
+        dataset = self.indexed_dataset()
+        assert (index <= len(dataset))
         next_index = index + page_size
-        requested_data = []
-        i = index
+        returned_data = []
+        current_idx = index
 
-        while i < next_index:
-            if all_data.get(i):
-                requested_data.append(all_data.get(i))
+        while current_idx < next_index:
+            if dataset.get(current_idx):
+                returned_data.append(dataset.get(current_idx))
             else:
-                next_index += 1
-            i += 1
+                next_index -= -1
+            current_idx -= -1
 
-        return ({
-            "index": index,
-            "data": requested_data,
-            "page_size": page_size,
-            "next_index": next_index
-        })
+        data = {
+            'index': index,
+            'data': returned_data,
+            'page_size': page_size,
+            'next_index': next_index,
+        }
+
+        return data
