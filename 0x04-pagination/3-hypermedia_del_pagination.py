5d7559bd0f0cf46ad1071c7b24e224cee09c9326
Diego Linares <diegojeanluck@hotmail.com>
2023-01-31 16:22:42 -0500

    Using other version

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List
 
 
 class Server:
@@ -39,38 +39,15 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Get page size elements from index, regardless if any were deleted
-
         Args:
             index (int, optional): Index to get. Defaults to None.
             page_size (int, optional): Size of Page. Defaults to 10.
-
         Returns:
             Dict: Data from index to index + page_size
         """
-        # all_data = self.indexed_dataset()
-        # total_len = len(all_data)
-        # assert (index <= total_len)
-        # next_index = index + page_size
-        # requested_data = []
-        # i = index
-
-        # while i < next_index:
-        #     if all_data.get(i):
-        #         requested_data.append(all_data.get(i))
-        #     else:
-        #         next_index += 1
-        #     i += 1
-
-        # return ({
-        #     "index": index,
-        #     "data": requested_data,
-        #     "page_size": page_size,
-        #     "next_index": next_index
-        # })
         dataset = self.indexed_dataset()
         assert (index <= len(dataset))
         next_index = index + page_size
