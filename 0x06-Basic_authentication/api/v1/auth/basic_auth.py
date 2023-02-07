6ab3c874293f63f8ba3ec696eb86904e8341ed45
Diego Linares <diegojeanluck@hotmail.com>
2023-02-07 02:09:50 -0500

    9. Basic - User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic Auth Module
 """
 
 from base64 import b64decode
+from sys import stderr
 
 from api.v1.auth.auth import Auth
 
@@ -61,3 +62,32 @@ class BasicAuth(Auth):
             return (b64decode(base64_authorization_header).decode('utf-8'))
         except Exception:
             return (None)
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        self descriptive
+
+        Args:
+            decoded_base64_authorization_header (str): info
+            of authorization header
+
+        Returns:
+            Tuple[str, str] | Tuple[None, None]: email and password
+            if exists, None otherwise
+        """
+
+        result = (None, None)
+
+        if (not decoded_base64_authorization_header):
+            return result
+
+        if (not isinstance(decoded_base64_authorization_header, str)):
+            return result
+
+        if (':' not in decoded_base64_authorization_header):
+            return result
+
+        result = tuple(decoded_base64_authorization_header.split(":"))
+
+        return result
