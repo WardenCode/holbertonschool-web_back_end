3eaf51d15035337f92db7b28b5891be3c08e26fc
Diego Linares <diegojeanluck@hotmail.com>
2023-02-07 00:50:37 -0500

    6. Basic auth

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Basic Auth Module
+"""
+
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Empty class of a Basic Auth
+
+    Args:
+        Auth (Auth): Main auth class
+    """
+    pass
