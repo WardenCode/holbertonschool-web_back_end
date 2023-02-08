36581aab7f2239f4af59c3d14dedab066eb3337b
Diego Linares <diegojeanluck@hotmail.com>
2023-02-08 15:24:01 -0500

    2. Create a session

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,9 @@
 Session Auth Module
 """
 
+from typing import Dict, Optional
+from uuid import uuid4
+
 from api.v1.auth.auth import Auth
 
 
@@ -13,4 +16,24 @@ class SessionAuth(Auth):
     Args:
         Auth (Auth): Main auth class
     """
-    pass
+
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(self, user_id: Optional[str] = None) -> Optional[str]:
+        """
+        Take a userID and creates a session
+
+        Args:
+            user_id (str, optional): UserID. Defaults to None.
+
+        Returns:
+            str: Session ID
+        """
+        if ((not isinstance(user_id, str)) or (not user_id)):
+            return (None)
+
+        Base: str = str(uuid4())
+
+        self.user_id_by_session_id[Base] = user_id
+
+        return (Base)
