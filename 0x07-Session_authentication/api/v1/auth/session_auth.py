35a3418f8771b5ffcb75a4c4eff70bad1a70568b
Diego Linares <diegojeanluck@hotmail.com>
2023-02-08 14:58:38 -0500

    1. Empty session

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Session Auth Module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session Auth class for the application
+
+    Args:
+        Auth (Auth): Main auth class
+    """
+    pass
