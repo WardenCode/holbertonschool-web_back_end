9ad48c67b90ab1f20d6c8e9ef23c31955a0155b2
Diego Linares <diegojeanluck@hotmail.com>
2023-02-23 18:04:03 -0500

    Using the correct configuration

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -14,11 +14,15 @@ babel = Babel(app)
 
 class Config(object):
     """
-    Config class for languages
+    Config class for available languages
     """
     LANGUAGES: List[str] = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE: str = 'en'
-    BABEL_DEFAULT_TIMEZONE: str = 'UTC'
+
+
+app.config.from_object(Config)
+
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
 
 
 @babel.localeselector
@@ -30,9 +34,6 @@ def get_locale() -> Optional[str]:
         Optional[str]: The best match in LANGUAGES variable
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-app.config.from_object(Config)
 
 
 @app.route('/', strict_slashes=False)
