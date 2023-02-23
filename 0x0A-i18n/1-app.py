88fd81ae19de433e6d8ee4b3e2e894f9cd57160c
Diego Linares <diegojeanluck@hotmail.com>
2023-02-23 17:11:50 -0500

    1. Basic Babel setup

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+1. Basic Babel setup
+"""
+
+from typing import List
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    Config class for languages
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'e   n'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def main_page():
+    """
+    _summary_
+    """
+    return (render_template('1-index.html'))
+
+
+if (__name__ == '__main__'):
+    app.run(debug=True)
