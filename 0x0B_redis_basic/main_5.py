843b2a88ee9793bc90a7394b50c54683c95ece06
Diego Linares <diegojeanluck@hotmail.com>
2023-03-06 21:37:45 -0500

    Mains

---
+++ 0x0B_redis_basic/main_5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from exercise import Cache, replay
+
+cache = Cache()
+
+cache = Cache()
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+replay(cache.store)
