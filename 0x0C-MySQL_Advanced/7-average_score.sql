abf5c72a7e1e894e53f7331925ecdc8de3ca4a6a
Diego Linares <diegojeanluck@hotmail.com>
2023-03-09 15:10:59 -0500

    Using sub-query instead variable

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -6,17 +6,8 @@ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE average DECIMAL;
-
-    SET average := (
-        SELECT AVG(score)
-        FROM corrections
-        WHERE corrections.user_id = user_id
-    );
-
     UPDATE users
-    SET average_score = average
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
     WHERE id = user_id;
 END $$
-
 DELIMITER ;
