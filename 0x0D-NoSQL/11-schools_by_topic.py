0d98e060d5018af2cccab61aa27c4a08868040a8
Diego Linares <diegojeanluck@hotmail.com>
2023-03-14 02:01:24 -0500

    11. Where can I learn Python?

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 """
-
+Search schools by topic
 """
 
+
+def schools_by_topic(mongo_collection, topic: str):
+    """
+    Search schools by topic
+
+    Args:
+        mongo_collection: MongoDB collection
+        topic (str): Topic to search
+    """
+    return mongo_collection.find({"topics": topic})
