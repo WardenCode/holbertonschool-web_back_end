abe3ebb20d33c7a08410b19ffff5663893407108
Diego Linares <diegojeanluck@hotmail.com>
2023-03-14 04:11:06 -0500

    14. Top students

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python3
 """
-
+Get all students sorted by average score
 """
 
+
+def top_students(mongo_collection):
+    """
+    Return all students sortered by score
+
+    Args:
+        mongo_collection: A mongoDB collection
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {
+                "averageScore": -1
+            }
+        }
+    ])
