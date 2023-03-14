90a6a5a5fc438a3e73e1cf95aedd48d9a8e57062
Diego Linares <diegojeanluck@hotmail.com>
2023-03-14 00:21:20 -0500

    8. List all documents in Python

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 """
-
+Lists all documents in a mongoDB collection
 """
 
+from typing import List
+
+
+def list_all(mongo_collection) -> List:
+    """
+    List all documents of a mongoDB collection
+
+    Args:
+        mongo_collection: MongoDB collection
+
+    Returns:
+        List: A list with all documents or an empty array
+    """
+    results = mongo_collection.find()
+
+    return [document for document in results]
