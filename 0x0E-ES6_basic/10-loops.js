2ae81e906f9fe74f4029313c6953b9c5b0c0e2c6
Diego Linares <diegojeanluck@hotmail.com>
2023-03-15 21:36:53 -0500

    10. For...of Loops

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue (array, appendString) {
+  let idx = 0;
+
+  for (const element of array) {
+    array[idx] = `${appendString}${element}`;
+    idx++;
+  }
+
+  return array;
+}
