c895d83bce8a90f83e018fdd9d09aa7cd34bdbba
Diego Linares <diegojeanluck@hotmail.com>
2023-03-16 20:47:45 -0500

    Use for of

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let idx = 0;
-
-  for (const element of array) {
-    array[idx] = `${appendString}${element}`;
-    idx++;
+  const array2 = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array2[idx] = appendString + value;
   }
 
-  return array;
+  return array2;
 }
