ea2c04061ce354e32831d35c3efcfc0bc2eeb2fc
Diego Linares <diegojeanluck@hotmail.com>
2023-03-16 20:01:04 -0500

    Remove unnecessary spaces on functions

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
 
   for (const element of array) {
