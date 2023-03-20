625fb8f70b64784ab4f02c99897aa7aac7dd89c9
Diego Linares <diegojeanluck@hotmail.com>
2023-03-20 04:39:46 -0500

    6. Handle multiple promises

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((promises) => promises.map(({ status, value }) => ({ status, value })));
+}
+
+export default handleProfileSignup;
