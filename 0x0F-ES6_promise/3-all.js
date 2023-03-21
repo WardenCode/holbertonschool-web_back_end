2d97841588817053ff92bfbbab34d67ffd2aa8e5
Diego Linares <diegojeanluck@hotmail.com>
2023-03-21 01:05:02 -0500

    Fix: using () on catch

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ function handleProfileSignup() {
     .then(([photo, user]) => {
       console.log(photo.body, user.firstName, user.lastName);
     })
-    .catch(console.log('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
