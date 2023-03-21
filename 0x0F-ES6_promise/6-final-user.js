ef50aa955c391a3936f9bcc905af098fac86c142
Diego Linares <diegojeanluck@hotmail.com>
2023-03-21 01:31:54 -0500

    Fix: Right use of the Promise.allSettled

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -5,7 +5,16 @@ function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((promises) => promises.map(({ status, value }) => ({ status, value })));
+  ]).then((promises) => {
+    const result = [];
+    for (const { status, value, reason } of promises) {
+      result.push({
+        status,
+        value: value || reason.toString(),
+      });
+    }
+    return result;
+  });
 }
 
 export default handleProfileSignup;
