2591f14c3171947da5b47de18fa928d2c5ff910a
Diego Linares <diegojeanluck@hotmail.com>
2023-03-23 00:18:47 -0500

    11. EVCar

---
+++ 0x10-ES6_classes/100-evcar.js
@@ -0,0 +1,20 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const clone = new (Object.getPrototypeOf(this.constructor))(
+      this._brand,
+      this._motor,
+      this._color,
+    );
+    clone._range = this._range;
+    return clone;
+  }
+}
+
+export default EVCar;
