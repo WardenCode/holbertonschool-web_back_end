aceb9c9e84f1cacad5de12101689c3ce8d1ac5a4
Diego Linares <diegojeanluck@hotmail.com>
2023-03-28 01:03:40 -0500

    11. Weak link data structure

---
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  const total = weakMap.get(endpoint) || 0;
+
+  weakMap.set(endpoint, total + 1);
+
+  if (total >= 5) throw new Error('Endpoint load is high');
+
+  return total;
+}
+
+export { queryAPI, weakMap };
