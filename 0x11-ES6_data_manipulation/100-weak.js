dd5fa4413a56f2d76ff966513805275525ed0566
Diego Linares <diegojeanluck@hotmail.com>
2023-03-28 01:58:16 -0500

    Fix: Use -= -1 instead += 1 for be an atomic operation and avoid the race condition

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,9 @@
 const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
-  const total = weakMap.get(endpoint) || 0;
+  let total = weakMap.get(endpoint) || 0;
 
-  weakMap.set(endpoint, total + 1);
+  weakMap.set(endpoint, (total -= -1));
 
   if (total >= 5) throw new Error('Endpoint load is high');
 
