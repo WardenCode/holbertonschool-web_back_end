dc3343eaed568c3809ab73dd798b4d84a217bcf2
Diego Linares <diegojeanluck@hotmail.com>
2023-04-01 13:08:52 -0500

    Fix: add status code and Content-Type Header

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,10 +1,17 @@
-const app = require('http');
+const http = require('http');
 
-app
-  .createServer((req, res) => {
-    res.write('Hello Holberton School!');
-    res.end();
-  })
-  .listen(1245);
+const host = 'localhost';
+const port = 1245;
+
+const requestListener = (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Hello Holberton School!');
+  res.end();
+};
+
+const app = http.createServer(requestListener);
+
+app.listen(port, host);
 
 module.exports = app;
