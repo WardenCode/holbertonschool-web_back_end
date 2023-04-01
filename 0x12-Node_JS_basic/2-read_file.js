6376abec26e96a58225c976cf02a073080a1c041
Diego Linares <diegojeanluck@hotmail.com>
2023-04-01 12:43:08 -0500

    2. Reading a file synchronously with Node JS

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let content = null;
+  try {
+    content = fs.readFileSync(path, 'utf-8');
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+
+  const students = content.split('\n');
+
+  let numberOfStudents = 0;
+  const studentsOnFields = {};
+
+  for (let i = 1; i < students.length; i += 1) {
+    const student = students[i];
+    if (!student) continue;
+
+    const [firstName, , , field] = students[i].split(',');
+
+    if (studentsOnFields[field]) {
+      studentsOnFields[field].push(firstName);
+    } else {
+      studentsOnFields[field] = [firstName];
+    }
+
+    numberOfStudents += 1;
+  }
+
+  console.log(`Number of students ${numberOfStudents}`);
+  for (const [key, value] of Object.entries(studentsOnFields)) {
+    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
