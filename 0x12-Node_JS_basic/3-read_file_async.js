3ffeb1a6881f1b5b02f13b9f05df5250a56fddb2
Diego Linares <diegojeanluck@hotmail.com>
2023-04-01 12:43:23 -0500

    3. Reading a file asynchronously with Node JS

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((content) => content.split('\n'))
+    .then((students) => {
+      let numberOfStudents = 0;
+      const studentsOnFields = {};
+
+      for (let i = 1; i < students.length; i += 1) {
+        const student = students[i];
+        if (!student) continue;
+
+        const [firstName, , , field] = students[i].split(',');
+
+        if (studentsOnFields[field]) {
+          studentsOnFields[field].push(firstName);
+        } else {
+          studentsOnFields[field] = [firstName];
+        }
+
+        numberOfStudents += 1;
+      }
+
+      console.log(`Number of students ${numberOfStudents}`);
+      for (const [key, value] of Object.entries(studentsOnFields)) {
+        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      }
+    })
+    .catch(() => {
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
