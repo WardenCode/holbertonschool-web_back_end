75411dac36bba0774928ebce6e64eaffb6553e33
Diego Linares <diegojeanluck@hotmail.com>
2023-04-02 20:58:09 -0500

    Fix: remove unnessesary logs and add title to error case

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -7,22 +7,15 @@ const app = express();
 
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 
 app.get('/students', async (req, res) => {
   const headerMsg = 'This is the list of our students\n';
   await countStudents(argv[2])
-    .then((data) => {
-      console.log(data);
-      res.send(`${headerMsg}${data}`);
-    })
-    .catch((err) => res.send(err.message));
+    .then((data) => res.send(`${headerMsg}${data}`))
+    .catch((err) => res.send(`${headerMsg}${err.message}`));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(port);
 
 module.exports = app;
