6dd5323221d0d24a45e333ba2948d515a8cf616b
Diego Linares <diegojeanluck@hotmail.com>
2023-04-14 01:26:38 -0500

    9. Track progress and errors with Kue: Create the Job processor

---
+++ 0x14-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,22 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const blockedPhoneNumbers = ['4153518780', '4153518781'];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+    job.progress(0, 50);
+
+    if (blockedPhoneNumbers.includes(phoneNumber))
+        return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+
+    job.progress(50, 100);
+
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message, job, done);
+    done();
+});
