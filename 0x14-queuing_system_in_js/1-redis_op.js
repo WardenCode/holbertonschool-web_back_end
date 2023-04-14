b5551f96c387504236c63b015bdc886b41d3c524
Diego Linares <diegojeanluck@hotmail.com>
2023-04-14 01:23:16 -0500

    2. Node Redis client and basic operations

---
+++ 0x14-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value, (err, cb) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        redis.print(`Reply: ${cb}`);
+    });
+}
+
+const displaySchoolValue = (schoolName) => {
+    client.get(schoolName, (err, value) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(value);
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
